9f71c9e09b292499ba877cdaa157b1d19cea6a89
aeroslav <aeroslav89@gmail.com>
2015-01-06 16:46:40 +0300

    2,3 - json, DOM; output list from json

--- js/2_json.js
+++ js/2_json.js
@@ -15,7 +15,7 @@ var books = [
             "description": "Windmills!"
         },
         {
-            "title": "Necronomicon",
+            "title": "Necronomicon: the Book of Grim Rituals",
             "author": "",
             "description": "Don't even touch bloody pages of this book!"
         },
@@ -27,37 +27,104 @@ var books = [
         {
             "title": "The Captain's Daughter",
             "author": "Alexander Pushkin",
-            "description": "Young lovers caught up in Pugachev’s peasant-Cossack revolt against Catherine the Great."
+            "description": "During the reign of Catherine the Great, the young Grinev sets out for his new career in the army and en route performs an act of kindness by giving his warm coat to a man freezing in a blizzard. This action reaps its reward when he subsequently finds himself caught up in the rebellion headed by the infamous, and strangely familiar, Pugachev. Rivalry with a fellow officer for the affections of Captain Mironov's daughter further complicates Grinev's affairs, and ultimately it is only an appeal by Masha Mironova, the eponymous captain's daughter, to the Empress herself that can unravel a tangled web."
         },
         {
             "title": "The Holy Quran",
             "author": "",
             "description": "Great philosophy, corrupted by extrimists in our days."
+        },
+        {
+            "title": "How to Draw Things",
+            "author": "Pablo Diego José Francisco de Paula Juan Nepomuceno María de los Remedios Cipriano de la Santísima Trinidad Ruiz y Picasso",
+            "description": "Actually, this book doesn't exists :)"
         }
     ],
-    app = {
-        iterateJSON: function (json) {
-            var listItems = '';
-            if ( !(json instanceof Object) ) {
+
+    printer = {
+        elName: null,
+        booksList: null,
+        truncStr: function(str,lng) {
+            if (str.length>lng) {
+                str = str.slice(0,lng) + '&hellip;';
+                return str;
+            } else
+                return str;
+        },
+        isObject: function (o) {
+            if (typeof o == 'object') {
+                if ( (o instanceof Array)||(o == null) ) {
+                    return false;
+                } else {
+                    return true;
+                };
+            } else
                 return false;
+        },
+        loadList: function(json){ //passed string parsed to JSON object and stored to inner var.
+            this.booksList = JSON.parse(json);
+        },
+        printRecord: function(el){
+            var df = document.createDocumentFragment(),
+                rec, title, author, descr;
+            if (this.isObject(el)&&(el.title !== '')) {
+                rec = document.createElement('li'),
+                title = document.createElement('span');
+                author = document.createElement('span');  //clone не работает!
+                descr = document.createElement('p');
+
+                console.log(el.title);
+                console.log(el.author);
+                console.log(el.descr);
+
+                title.innerHTML = this.truncStr(el.title,25);
+                author.innerHTML = this.truncStr(el.author,25);
+                descr.innerHTML = this.truncStr(el.description,150);
+
+                title.classList.add('book-title');
+                author.classList.add('book-author');
+                descr.classList.add('book-descr');
+                rec.classList.add('book', 'cf');
+
+                rec.appendChild(title);
+                rec.appendChild(author);
+                rec.appendChild(descr);
+                df.appendChild(rec);
+
+                return df;
+            } else
+                return false;
+        },
+        formList: function() {
+            var list = document.createDocumentFragment();
+            this.booksList.forEach(function(el) {
+                var record = this.printRecord(el);
+                if (record) {
+                    list.appendChild(record);
+                }
+            }, this);
+            return list;
+        },
+        sendToDOM: function(node){
+            var where = this.elName;
+            if (node instanceof Node) {
+                where.appendChild(node);
+            }
+        },
+        renderToDOM: function() {
+            if (this.booksList !== null) {
+                this.sendToDOM(this.formList());
+                return true;
             } else {
-                listItems = json.reduce(function(list,el){
-                    if (el.title !== '') {
-                        list += '<li class="book cf">\n'+
-                            '<span class="book-title">'+el.title+'</span>\n'+
-                            '<span class="book-author">'+(el.author?el.author:'Unknown')+'</span>\n'+
-                            '<p class="book-descr">'+(el.description?el.description:'No description')+'</p></li>'+'\n';
-                    };
-                    return list;
-                },'');
-            };
-            return listItems;
+                console.log('booksList is empty');
+                return false;
+            }
         }
     },
-    booksStr = JSON.stringify(books),
-    booksJSON = JSON.parse(booksStr);
 
-/*console.log(booksStr);
-console.log(booksJSON);*/
+    booksStr = JSON.stringify(books);
 
-document.querySelector('.bookslist').innerHTML = '<ul>'+app.iterateJSON(booksJSON)+'</ul>';
+//------------
+printer.elName = document.querySelector('.bookslist');
+printer.loadList(booksStr);
+printer.renderToDOM();
